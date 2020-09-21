8f094d27f16bbb093c906f07ad923d090827df2f
CAndrew-Code <69196898+CAndrew-Code@users.noreply.github.com>
2020-09-20 22:09:05 -0400

    Add files via upload

---
+++ Checking Task Status/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Checking_Task_Status
+{
+    class Program
+    {
+        static Timer time;
+
+        static Task task1;
+        static void Main(string[] args)
+        {
+            task1 = new Task(() =>
+            {
+                for (int i = 1; i <= 500; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            });
+            task1.Start();
+            time = new Timer();
+            time.Elapsed += CheckTheTask;
+            time.Interval = 100;
+            time.Enabled = true;
+            
+            Console.ReadLine();
+        }
+
+        private static void CheckTheTask(object sender, ElapsedEventArgs e)
+        {
+            if (task1.Status == TaskStatus.Running)
+            {
+                Console.WriteLine("Task is running...");
+            }
+            else if (task1.Status == TaskStatus.RanToCompletion)
+            {
+                Console.WriteLine("Task is complete.");
+                time.Enabled = false;
+            }
+        }
+    }
+}
---
+++ Concurrent Collections/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Concurrent_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcurrentBag<int> bag = new ConcurrentBag<int>();
+
+            Thread t1 = new Thread(() =>
+            {
+                Console.WriteLine("Thread 1 started\n");
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine("Thread 1 added " + i);
+                    bag.Add(i);
+                }
+                Console.WriteLine("Thread 1 Complete.\n");
+            });
+
+            Thread t2 = new Thread(() =>
+            {
+                Console.WriteLine("Thread 2 started\n");
+                for (int i = 15; i <= 25; i++)
+                {
+                    Console.WriteLine("Thread 2 added " + i);
+                    bag.Add(i);
+                }
+                Console.WriteLine("Thread 2 Complete.\n");
+            });
+
+            Thread t3 = new Thread(() =>
+            {
+                t1.Join();
+                t2.Join();
+                Console.WriteLine("Thread 3 Started\n");
+                foreach (var item in bag)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.WriteLine("Thread 3 Complete\n");
+            });
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+        }
+    }
+}
