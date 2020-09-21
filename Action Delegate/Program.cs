4c2255f5746e5883a60aa4ac1a110e820b79ab87
CAndrew-Code <69196898+CAndrew-Code@users.noreply.github.com>
2020-09-20 21:59:54 -0400

    Add files via upload

---
+++ Action Delegate/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Action_Delegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<int> playerRank = PlayerSn;
+            playerRank(58);
+
+            Action playerName = PlayerSN;
+            playerName();
+        }
+
+        public static void PlayerSn (int rank)
+        {
+            Console.WriteLine("Your Player rank is " + rank);
+        }
+
+        public static void PlayerSN()
+        {
+            Console.WriteLine("Baberham Lincoln");
+        }
+    }
+}
---
+++ Anonnomous Methods/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousMethods
+{
+    delegate void DisplayInfo();
+    delegate void DisplayInfoBasedOnNumber(int number);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayInfo normalDelegate = new DisplayInfo(GetInformation);
+            normalDelegate();
+
+            DisplayInfoBasedOnNumber oneParameterDelegate = new DisplayInfoBasedOnNumber(GetInformation);
+            oneParameterDelegate(100);
+
+            DisplayInfo delUsingAnonymousMethod = delegate { Console.WriteLine("Hello we are Anonymous!"); };
+            delUsingAnonymousMethod();
+
+            DisplayInfoBasedOnNumber anonymousDelWithParameter = delegate (int number) { Console.WriteLine("We are still Anonymous " + number); };
+            anonymousDelWithParameter(10);
+        }
+
+        public static void GetInformation()
+        {
+            // Some logic goes in here
+            Console.WriteLine("Delegate with no parameter");
+        }
+
+        public static void GetInformation(int x)
+        {
+            // Some logic goes in here
+            Console.WriteLine("Delegate with one parameter : " + x);
+        }
+    }
+}
